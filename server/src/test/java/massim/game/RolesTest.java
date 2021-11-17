f6690cf208d869f4a04bc11a51f8aec0ed3d01e6
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2021-11-17 14:48:00 +0100

    more test

--- server/src/test/java/massim/game/RolesTest.java
+++ server/src/test/java/massim/game/RolesTest.java
@@ -1,13 +1,17 @@
 package massim.game;
 
 import massim.config.TeamConfig;
+import massim.game.environment.positionable.Entity;
 import massim.game.environment.zones.Zone;
 import massim.game.environment.zones.ZoneType;
 import massim.helper.ConfigBuilder;
 import massim.protocol.data.Position;
+import massim.protocol.data.Role;
+import massim.protocol.messages.scenario.ActionResults;
 import massim.util.RNG;
 import org.json.JSONObject;
 
+import java.util.List;
 import java.util.Set;
 
 public class RolesTest {
@@ -24,10 +28,11 @@ public class RolesTest {
     }
 
     @org.junit.Test
-    public void testRoleZones() {
+    public void testRoles() {
         var number = 5;
         var size = 3;
         ConfigBuilder.setRoleZones(config, number, size);
+        ConfigBuilder.addRole(config, new Role("testRole", 5, Set.of("adopt"), new int[]{3,2,1}));
         var state = new GameState(config, teams);
 
         var zones = state.getGrid().getZones(ZoneType.ROLE);
@@ -38,5 +43,24 @@ public class RolesTest {
             assert zone.position() != null;
             assert zone.toJSON() != null;
         }
+
+        Zone testZone = zones.get(0);
+        var zoneCenter = testZone.position();
+        Entity a1 = state.getGrid().entities().getByName("A1");
+
+        assert state.handleAdoptAction(a1, "unknownRole").equalsIgnoreCase(ActionResults.FAILED_PARAMETER);
+
+        assert state.teleport("A1", zoneCenter.moved("e", size + 1));
+        assert state.handleAdoptAction(a1, "testRole").equalsIgnoreCase(ActionResults.FAILED_LOCATION);
+
+        assert state.teleport("A1", zoneCenter);
+        assert state.handleAdoptAction(a1, "testRole").equalsIgnoreCase(ActionResults.SUCCESS);
+
+        var position = a1.getPosition();
+        assert state.handleMoveAction(a1, List.of("e", "e", "e")).equals(ActionResults.SUCCESS);
+        assert a1.getPosition().equals(position.moved("e", 3));
+
+        position = a1.getPosition();
+
     }
 }
